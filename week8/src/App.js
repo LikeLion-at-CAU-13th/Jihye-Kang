95cfdc3cd67673aa9f6d2e787aed14066b08ac22
Jihaeee <elinkang02@gmail.com>
2025-05-20 18:10:17 +0900

    오류 수정

--- week8/src/App.js
+++ week8/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header/Header';
-import NavBar from './components/Navbar/Navbar';
+import Header from './components/Header/Header.jsx';
 
-import HomePage from './pages/HomePage/HomePage';
-import InfoPage from './pages/InfoPage/InfoPage';
-import ContactPage from './pages/ContactPage/ContactPage';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import InfoPage from './pages/InfoPage/InfoPage.jsx';
+import ContactPage from './pages/ContactPage/ContactPage.jsx';
 
 function App() {
+
+  console.log('HomePage:', HomePage);
+  console.log('InfoPage:', InfoPage);
+  console.log('ContactPage:', ContactPage);
+
   return (
     <BrowserRouter>
       <Header />
-      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<InfoPage />} />
